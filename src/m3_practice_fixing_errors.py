b2af90e0012ceb9709060be3e62e7e37a4d41b8b
DavidMutchler <mutchler@rose-hulman.edu>
2018-09-02 19:58:19 -0400

    Made the instructions slightly more precise.

--- src/m3_practice_fixing_errors.py
+++ src/m3_practice_fixing_errors.py
@@ -25,7 +25,7 @@ Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
 #    Finish by RUNNING the corrected program
 #    and making sure that it RUNS CORRECTLY.
 #      That is, make sure that (per the doc-strings) the program
-#      prints some calculated values and makes a Turtle do some things.
+#      prints two calculated values and makes a SimpleTurtle do some things.
 #
 #   When finished, COMMIT-and-PUSH your work, as always.
 #
